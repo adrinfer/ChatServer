cbec21a6187a8cb0ab84473ec27d273b8e726a94
Practicas01 <Practicas01@DESKTOP-2ISAPKN>
2016-04-20 14:59:45 +0200

    cracion...scrollBar.css, Utils.java, cambio de nombres

--- src/chatserver/Main.java
+++ src/chatserver/Main.java
@@ -5,12 +5,15 @@
  */
 package chatserver;
 
+import java.beans.PersistenceDelegate;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 
 /**
  *
@@ -18,15 +21,38 @@ import javafx.stage.StageStyle;
  */
 public class Main extends Application {
     
+    
+    private BorderPane root;
+    private Scene scene;
+    
+    private void setBindings()
+    {
+        //Width
+        root.minWidthProperty().bind(scene.widthProperty().subtract(1));
+        root.maxWidthProperty().bind(scene.widthProperty().subtract(1));
+        root.prefWidthProperty().bind(scene.widthProperty().subtract(1));
+        
+        //Height
+        root.minHeightProperty().bind(scene.heightProperty().subtract(1));
+        root.maxHeightProperty().bind(scene.heightProperty().subtract(1));
+        root.prefHeightProperty().bind(scene.heightProperty().subtract(1));
+    }
+    
+    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/es/chatserver/views/FXMLDocument.fxml"));
+        root = FXMLLoader.load(getClass().getResource("/es/chatserver/views/FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/es/chatserver/styles/scrollBar.css").toExternalForm());
+
         stage.setTitle("AAAA");
-        stage.initStyle(StageStyle.UTILITY);
+        //stage.initStyle(StageStyle.UTILITY);
         stage.setScene(scene);
         stage.show();
+        
+        setBindings();
+        
     }
 
     /**
---
+++ src/es/chatserver/controllers/viewcontrollers/ServerGuiController.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.chatserver.controllers.viewcontrollers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author adrinfer
+ */
+public class ServerGuiController implements Initializable {
+    
+    @FXML
+    private BorderPane borderPane;
+        
+    @FXML
+    private HBox topPane;
+    
+    @FXML
+    private BorderPane bottomPane;
+    
+    @FXML
+    private VBox leftPane;
+    
+    @FXML
+    private VBox rightPane;
+    
+    @FXML
+    private TextArea textArea;
+    
+    @FXML
+    private TitledPane titledPaneUsers;
+    
+    @FXML
+    private VBox vBoxUserList;
+   
+    @FXML
+    private Label lblStatus;
+
+    
+    @FXML
+    private ScrollPane scrollPaneUsers;
+    
+    @FXML
+    private ToggleButton butStartStop;
+    
+    
+    
+    
+    
+    private void startStopButton()
+    {
+                
+//        if(server != null)
+//        {
+//            server.stop();
+//            server = null;
+//            butStartStop.setText("Start");
+//            textArea.setText(textArea.getText() + "\n" + "*** Server: Stop");
+//            lblStatus.setText("Server: Parado");
+//            
+//            
+//        }
+//        else
+//        {
+//           
+//            butStartStop.setText("Stop");
+//            textArea.setText(textArea.getText() + "\n" + "*** Server: Start");
+//            lblStatus.setText("Server: Iniciado");
+//        }
+    }
+    
+
+    
+    private void setBindings()
+    {
+        
+        //leftPane - widthProperty
+        leftPane.minWidthProperty().bind(borderPane.widthProperty().multiply(0.2).subtract(100));
+        leftPane.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.2).subtract(100));
+        leftPane.prefWidthProperty().bind(borderPane.widthProperty().multiply(0.2).subtract(100));
+        
+        
+        //leftPane - heightProperty
+        leftPane.minHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        leftPane.maxHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        leftPane.prefHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        
+        
+        //rightPane - width binding
+        rightPane.minWidthProperty().bind(borderPane.widthProperty().multiply(0.2));
+        rightPane.maxWidthProperty().bind(borderPane.widthProperty().multiply(0.2));
+        rightPane.prefWidthProperty().bind(borderPane.widthProperty().multiply(0.2));
+        
+        //rightPane - heightProperty
+        rightPane.minHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        rightPane.maxHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        rightPane.prefHeightProperty().bind(borderPane.heightProperty().subtract(topPane.heightProperty().add(bottomPane.heightProperty())));
+        
+  
+        //lblTitleUsers - width binding
+        titledPaneUsers.minWidthProperty().bind(leftPane.widthProperty());
+        titledPaneUsers.maxWidthProperty().bind(leftPane.widthProperty());
+        titledPaneUsers.prefWidthProperty().bind(leftPane.widthProperty());
+        
+        
+        //vBox que almacena los label del listado de usuarios (en el leftPane)
+        vBoxUserList.minWidthProperty().bind(scrollPaneUsers.widthProperty());
+        vBoxUserList.maxWidthProperty().bind(scrollPaneUsers.widthProperty());
+        vBoxUserList.prefWidthProperty().bind(scrollPaneUsers.widthProperty());
+        
+        
+        //scrollPaneUsers - height binding - contiene la lista de usuarios
+//        scrollPaneUsers.minHeightProperty().bind(leftPane.heightProperty().subtract( titlePaneUsersList.heightProperty() ));
+//        scrollPaneUsers.maxHeightProperty().bind(leftPane.heightProperty().subtract( titlePaneUsersList.heightProperty() ));
+//        scrollPaneUsers.prefHeightProperty().bind(leftPane.heightProperty().subtract( titlePaneUsersList.heightProperty() ));
+//        
+    }
+    
+    
+    private void init()
+    {
+        
+        textArea.setEditable(false);
+        setBindings();
+
+//      leftPane.setAlignment(Pos.BASELINE_RIGHT);
+
+        scrollPaneUsers.setHbarPolicy(ScrollBarPolicy.NEVER);
+
+
+        lblStatus.setText("Server: Parado");
+        butStartStop.setText("Start");
+        //Listener startStop server button
+        butStartStop.setOnAction((event) -> {
+            startStopButton();        
+        });
+        
+        Label l;
+        for(int x = 0; x <= 50; x++)
+        {
+            l = new Label("Texto label");
+            l.setPadding(new Insets(0, 0, 0, 20));
+            l.setStyle("-fx-background-color: white;");
+            vBoxUserList.getChildren().add(l);
+        }
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        init();
+    }    
+    
+}
